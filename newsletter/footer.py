9b59458f711388b446d99ce7ff521afe0bbc7bba
Zitian Qin <z5476244@ad.unsw.edu.au>
2024-10-15 19:00:09 +1100

    Update sponsors image

--- newsletter/footer.py
+++ newsletter/footer.py
@@ -2,7 +2,7 @@ from newsletter.styles import DARK_GREY, SOCIALS
 from newsletter.components import add_section_header
 
 
-SPONSORS_IMAGE = "https://i.imgur.com/J2tRMZO.png"
+SPONSORS_IMAGE = "https://i.imgur.com/fu1LOE4.png"
 
 LINKS = {
     "Facebook Page": "https://www.facebook.com/csesoc",
